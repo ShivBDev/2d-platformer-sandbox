0254873ff8c0ab21eb4127a639c3b909b2e29854
Shiv B <dev@shivb.design>
2026-05-27 22:51:00 -0700

    Added Floating Platform Object Updated Basic World Layout Added Landing Animation Adjusted Default Player Movement values

--- Scripts/character_controller.gd
+++ Scripts/character_controller.gd
@@ -7,19 +7,18 @@ extends CharacterBody2D
 @export var RUN_SPEED				: float = 200.0
 @export var RUN_ACC					: float = 500.0
 @export var RUN_STICK_THRESHOLD		: float = 0.2
-@export var IDLE_SPEED_THRESHOLD	: float = 5.0
-@export var GROUND_FRICTION			: float = 800.0
+@export var IDLE_SPEED_THRESHOLD		: float = 5.0
+@export var GROUND_FRICTION			: float = 1000.0
 @export_subgroup("Aerial Values")
 @export var JUMP_VELOCITY			: float = -150.0
 @export var JUMP_ACC					: float = -750.0
 @export var MAX_JUMP_TIME			: float = 0.2 # in seconds
-@export var DRIFT_SPEED				: float = 240.0
-@export var DRIFT_ACC					: float = 160.0
+@export var DRIFT_SPEED				: float = 200.0
+@export var DRIFT_ACC				: float = 100.0
 @export_range(0.0, 0.5, 0.05, "suffix:Seconds") var COYOTE_TIME : float = 0.1
 @export_subgroup("Misc Debug Settings")
 @export_range(1.0, 3.0, 0.1) var JUMP_SQUAT_GRAVITY_DAMP : float = 1.5
 # SUBREGION Calculated Variables, based on exported variable values
-var REDUCED_FRICTION		: float = GROUND_FRICTION / 2.0
 var REDUCED_GRAVITY		: Vector2 = get_gravity() / JUMP_SQUAT_GRAVITY_DAMP
 #endregion consts
 
@@ -31,12 +30,14 @@ var _coyoteTimer 	: float = 0.0
 var _jumpTimer 	: float = 0.0
 var _jump_squat_last_frame_index : int = 0
 var _jump_held : bool = true
+var _landing_anim_last_frame_index : int = 0
 @onready var sprite	: AnimatedSprite2D = $Sprite
 #endregion state_management
 
 #region control_flow
 func _ready() -> void:
 	_jump_squat_last_frame_index = sprite.sprite_frames.get_frame_count("jump_squat") - 1
+	_landing_anim_last_frame_index = sprite.sprite_frames.get_frame_count("landing") - 1
 
 func _physics_process(delta: float) -> void:
 	_stickDir = Input.get_axis("Left", "Right")
@@ -53,7 +54,7 @@ func _switch_state(state: String) -> void:
 	match state:
 		"run": _apply_run_speed()
 		"idle": _apply_ground_friction()
-		"fall": _apply_gravity()
+		"fall": _coyoteTimer = 0.0
 		"jump_squat":
 			_apply_ground_friction(true) #apply reduced friction for jump squat
 			_jump_held = true
@@ -61,6 +62,8 @@ func _switch_state(state: String) -> void:
 			velocity.y = JUMP_VELOCITY
 			_jumpTimer = 0.0
 		"jump", "fall": _apply_drift_speed()
+		"landing":
+			_apply_ground_friction()
 		_: pass # Some states don't require anything extra
 	sprite.play(state)
 #endregion control_flow
@@ -69,24 +72,15 @@ func _switch_state(state: String) -> void:
 # TODO: Seems like a simple function, but will later need to differentiate double jump and jump
 func _handle_jump_press() -> void:
 	if Input.is_action_just_pressed("Jump"):
-		_switch_state("jump_squat")
-
-func _handle_coyote_time() -> void:
-	if is_on_floor():
-		_coyoteTimer = 0.0
-		return
-	if _coyoteTimer >= COYOTE_TIME:
-		return _switch_state("fall")
-	_coyoteTimer += _engineDelta
+		_switch_state("jump_squat" if is_on_floor() else "jump")
 
 ## Gravity
 func _apply_gravity(reduced : bool = false) -> void:
 	var gravity : Vector2 = REDUCED_GRAVITY if reduced else get_gravity()
 	velocity += gravity * _engineDelta
 ## Slowly reduce player movement
-func _apply_ground_friction(reduced : bool = false) -> void:
-	var friction : float = REDUCED_FRICTION if reduced else GROUND_FRICTION
-	velocity.x = move_toward( velocity.x, 0, friction * _engineDelta)
+func _apply_ground_friction(gravScale : float = 1.0) -> void:
+	velocity.x = move_toward(velocity.x, 0, GROUND_FRICTION * gravScale * _engineDelta)
 ## Applies lateral run speed (on ground)
 func _apply_run_speed() -> void:
 	velocity.x = move_toward(velocity.x, RUN_SPEED * _stickDir, RUN_ACC * _engineDelta)
@@ -107,7 +101,7 @@ func _set_facing_dir() -> void:
 func _idle_state() -> void:
 	# by checking jump before floor check, we add 1 hidden frame of coyote time
 	_handle_jump_press()
-	_handle_coyote_time()
+	if not is_on_floor(): return _switch_state("fall")
 	if abs(_stickDir) > RUN_STICK_THRESHOLD: return _switch_state("run")
 	_set_facing_dir()
 	_apply_ground_friction()
@@ -115,7 +109,7 @@ func _idle_state() -> void:
 func _run_state() -> void:
 	# by checking jump before floor check, we add 1 hidden frame of coyote time
 	_handle_jump_press()
-	_handle_coyote_time()
+	if not is_on_floor(): return _switch_state("fall")
 	if absf(_stickDir) < RUN_STICK_THRESHOLD: return _switch_state("idle")
 	_set_facing_dir()
 	_apply_run_speed()
@@ -124,12 +118,12 @@ func _jump_squat_state() -> void:
 	if sprite.frame == _jump_squat_last_frame_index and not sprite.is_playing():
 		return _switch_state("jump")
 	# Apply reduced friction and gravity to keep momentum and height for jump
-	_apply_ground_friction(true)
+	_apply_ground_friction(0.5)
 	_apply_gravity(true)
 	_handle_jump_hold_check()
 
 func _jump_state() -> void:
-	if is_on_floor(): return _switch_state("idle")
+	if is_on_floor(): return _switch_state("landing")
 	if velocity.y > 0: return _switch_state("fall")
 	_handle_jump_hold_check()
 	if _jump_held and _jumpTimer <= MAX_JUMP_TIME:
@@ -140,8 +134,18 @@ func _jump_state() -> void:
 	_apply_gravity()
 
 func _fall_state() -> void:
-	if is_on_floor(): return _switch_state("idle")
+	if is_on_floor(): return _switch_state("landing")
 	_set_facing_dir()
 	_apply_drift_speed()
-	_apply_gravity()
+	#coyote time!
+	if _coyoteTimer < COYOTE_TIME:
+		_coyoteTimer += _engineDelta
+		_handle_jump_press()
+	else:
+		_apply_gravity()
+
+func _landing_state() -> void:
+	if sprite.frame == _landing_anim_last_frame_index and not sprite.is_playing():
+		return _switch_state("idle")
+	_apply_ground_friction()
 #endregion
