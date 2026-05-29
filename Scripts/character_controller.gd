294bcc69c58c1832eef82d8e0b7e119bbecf6863
Shiv B <dev@shivb.design>
2026-05-28 17:58:51 -0700

    Added God Mode Add 2pc and 3pc variations of floating platform Added Wall Hold and Wall Jump More Modifications to player movement

--- Scripts/character_controller.gd
+++ Scripts/character_controller.gd
@@ -1,5 +1,24 @@
 extends CharacterBody2D
 
+#DEBUG_TOOL
+const godMode = "_god_mode"
+@onready var collider: CollisionShape2D = $CollisionShape2D
+func _handle_god_mode() -> void:
+	if not Input.is_action_just_pressed("GodModeToggle"): return
+	if sprite.animation == godMode:
+		print("Exiting God Mode")
+		collider.disabled = false
+		return _switch_state("idle")
+	print("Entering God Mode")
+	collider.disabled = true
+	return _switch_state(godMode)
+func __god_mode_state() -> void:
+	_set_facing_dir()
+	var verticalInput : float = Input.get_axis("Down", "Up")
+	var inputVec : Vector2 = Vector2(_stickDir, -verticalInput).normalized()
+	velocity = inputVec * RUN_SPEED * 2
+#DEBUG_TOOL end
+
 #region consts
 # SUBREGION Exported Variables, default values set here, can be updated in editor
 @export_group("Character Controller Movement Values")
@@ -11,25 +30,40 @@ extends CharacterBody2D
 @export var GROUND_FRICTION			: float = 1000.0
 @export_subgroup("Aerial Values")
 @export var JUMP_VELOCITY			: float = -150.0
-@export var JUMP_ACC					: float = -750.0
+@export var JUMP_ACC					: float = -1500.0
 @export var MAX_JUMP_TIME			: float = 0.2 # in seconds
 @export var DRIFT_SPEED				: float = 200.0
-@export var DRIFT_ACC				: float = 100.0
+@export var DRIFT_ACC				: float = 150.0
 @export_range(0.0, 0.5, 0.05, "suffix:Seconds") var COYOTE_TIME : float = 0.1
-@export_subgroup("Misc Debug Settings")
-@export_range(1.0, 3.0, 0.1) var JUMP_SQUAT_GRAVITY_DAMP : float = 1.5
-# SUBREGION Calculated Variables, based on exported variable values
-var REDUCED_GRAVITY		: Vector2 = get_gravity() / JUMP_SQUAT_GRAVITY_DAMP
+@export_subgroup("Misc Physics Settings")
+@export var MAX_FALL_SPEED						: float = 1000.0
+@export var MAX_WALL_HOLD_FALL_SPEED 			: float = 100.0
+@export var MAX_WALL_HOLD_INIT_UPWARD_SPEED 		: float = -100.0
+@export var WALL_HOLD_DRIFT_SPEED				: float = 5.0
+@export var WALL_HOLD_LATERAL_SPEED_THRESHOLD 	: float = 50.0
+@export var WALL_HOLD_LOCKOUT_TIME				: float = 0.2
+@export_range(0.0, 2.0, 0.05) var WALL_JUMP_LATERAL_SPEED_SCALE		: float = 1.0
+@export_range(0.0, 1.0, 0.05) var WALL_HOLD_GRAVITY_SCALE				: float = 0.25
+@export_range(0.0, 1.0, 0.05) var JUMP_SQUAT_GRAVITY_SCALE			: float = 0.5
+@export_range(0.0, 1.0, 0.05) var JUMP_SQUAT_GROUND_FRICTION_SCALE	: float = 0.5
+@export_range(1.0, 5.0, 0.1) var LANDING_GROUND_FRICTION_SCALE		: float = 1.5
 #endregion consts
 
 #region state_management
-var _stickDir 	: float = 0.0
+# SUBREGION Engine Data
+var _stickDir 		: float = 0.0
 var _engineDelta 	: float = 0.0
+# SUBREGION Player Info
 var _facingRight 	: bool = true
 var _coyoteTimer 	: float = 0.0
-var _jumpTimer 	: float = 0.0
+var _jumpTimer 		: float = 0.0
+var _jump_held 		: bool = true
+var _disable_turn_till_fall : bool = false
+var _wall_to_right 	: bool = true
+var _wall_hold_lockout_timer : float = 0.0
+var _prev_frame_vel : Vector2 = Vector2.ZERO
+# SUBREGION Anim Info
 var _jump_squat_last_frame_index : int = 0
-var _jump_held : bool = true
 var _landing_anim_last_frame_index : int = 0
 @onready var sprite	: AnimatedSprite2D = $Sprite
 #endregion state_management
@@ -43,10 +77,13 @@ func _physics_process(delta: float) -> void:
 	_stickDir = Input.get_axis("Left", "Right")
 	_engineDelta = delta
 
+	_handle_god_mode() #DEBUG_TOOL
+
 	var stateFn = Callable(self, "_%s_state" % [sprite.animation])
 	if stateFn.is_valid(): stateFn.call()
 	else: push_error("Character FSM Missing Func for: %s" % [sprite.animation])
 
+	_prev_frame_vel = velocity
 	move_and_slide()
 
 ## Handle common tasks done before switching to a specific state.
@@ -56,31 +93,65 @@ func _switch_state(state: String) -> void:
 		"idle": _apply_ground_friction()
 		"fall": _coyoteTimer = 0.0
 		"jump_squat":
-			_apply_ground_friction(true) #apply reduced friction for jump squat
+			_apply_ground_friction(JUMP_SQUAT_GROUND_FRICTION_SCALE) #apply reduced friction for jump squat
 			_jump_held = true
 		"jump":
 			velocity.y = JUMP_VELOCITY
 			_jumpTimer = 0.0
 		"jump", "fall": _apply_drift_speed()
 		"landing":
-			_apply_ground_friction()
+			_apply_ground_friction(LANDING_GROUND_FRICTION_SCALE)
+		"wall_hold":
+			if velocity.y < 0 : velocity.y = max(velocity.y, MAX_WALL_HOLD_INIT_UPWARD_SPEED)
+			elif velocity.y > 0 : velocity.y = min(velocity.y, MAX_WALL_HOLD_FALL_SPEED)
 		_: pass # Some states don't require anything extra
 	sprite.play(state)
 #endregion control_flow
 
 #region helpers
-# TODO: Seems like a simple function, but will later need to differentiate double jump and jump
-func _handle_jump_press() -> void:
-	if Input.is_action_just_pressed("Jump"):
-		_switch_state("jump_squat" if is_on_floor() else "jump")
+func _handle_jump_press() -> bool:
+	if not Input.is_action_just_pressed("Jump"): return false
+	_wall_hold_lockout_timer = 0.0
+	if is_on_floor() and velocity.y == 0:
+		_switch_state("jump_squat")
+		return true
+	_jump_held = true # allow variable jump height for coyote/wall jumps
+	if is_on_wall():
+		var dir = -1.0 if _wall_to_right else 1.0
+		velocity.x = absf(JUMP_VELOCITY) * WALL_JUMP_LATERAL_SPEED_SCALE * dir
+		_disable_turn_till_fall = false
+		_set_facing_dir(false)
+		_disable_turn_till_fall = true
+	_switch_state("jump")
+	return true
+
+func _handle_wall_hold_check() -> bool:
+	if _wall_hold_lockout_timer < WALL_HOLD_LOCKOUT_TIME :
+		_wall_hold_lockout_timer += _engineDelta
+		return false
+	if is_on_wall():
+		print("ON WALL: ", velocity, _prev_frame_vel)
+		_wall_to_right = true if get_wall_normal().x < 0 else false
+		if _wall_to_right:
+			if Input.is_action_pressed("Right") or _prev_frame_vel.x > WALL_HOLD_LATERAL_SPEED_THRESHOLD:
+				_switch_state("wall_hold")
+				return true
+		else:
+			if Input.is_action_pressed("Left") or _prev_frame_vel.x < -WALL_HOLD_LATERAL_SPEED_THRESHOLD:
+				_switch_state("wall_hold")
+				return true
+	return false
+
 
 ## Gravity
-func _apply_gravity(reduced : bool = false) -> void:
-	var gravity : Vector2 = REDUCED_GRAVITY if reduced else get_gravity()
-	velocity += gravity * _engineDelta
+func _apply_gravity(gravityScale : float = 1.0, maxFallSpeed : float = MAX_FALL_SPEED) -> void:
+	var gravity : Vector2 = get_gravity() * gravityScale * _engineDelta
+	velocity += gravity
+	velocity.y = minf(maxFallSpeed, velocity.y)
 ## Slowly reduce player movement
-func _apply_ground_friction(gravScale : float = 1.0) -> void:
-	velocity.x = move_toward(velocity.x, 0, GROUND_FRICTION * gravScale * _engineDelta)
+func _apply_ground_friction(frictionScale : float = 1.0) -> void:
+	var friction : float = GROUND_FRICTION * frictionScale * _engineDelta
+	velocity.x = move_toward(velocity.x, 0, friction)
 ## Applies lateral run speed (on ground)
 func _apply_run_speed() -> void:
 	velocity.x = move_toward(velocity.x, RUN_SPEED * _stickDir, RUN_ACC * _engineDelta)
@@ -91,8 +162,12 @@ func _apply_drift_speed() -> void:
 func _handle_jump_hold_check():
 	_jump_held = Input.is_action_pressed("Jump") if _jump_held else false
 ## Track facing direction for sprites
-func _set_facing_dir() -> void:
-	_facingRight = _facingRight if _stickDir == 0 else _stickDir > 0
+func _set_facing_dir(stickBased:bool = true) -> void:
+	if _disable_turn_till_fall: return
+	if stickBased:
+		_facingRight = _facingRight if _stickDir == 0 else _stickDir > 0
+	else:
+		_facingRight = velocity.x > 0
 	# TODO: Have independent sprites for left and right facing?
 	sprite.flip_h = not _facingRight
 #endregion helpers
@@ -100,7 +175,7 @@ func _set_facing_dir() -> void:
 #region states
 func _idle_state() -> void:
 	# by checking jump before floor check, we add 1 hidden frame of coyote time
-	_handle_jump_press()
+	if _handle_jump_press() : return
 	if not is_on_floor(): return _switch_state("fall")
 	if abs(_stickDir) > RUN_STICK_THRESHOLD: return _switch_state("run")
 	_set_facing_dir()
@@ -108,7 +183,7 @@ func _idle_state() -> void:
 
 func _run_state() -> void:
 	# by checking jump before floor check, we add 1 hidden frame of coyote time
-	_handle_jump_press()
+	if _handle_jump_press(): return
 	if not is_on_floor(): return _switch_state("fall")
 	if absf(_stickDir) < RUN_STICK_THRESHOLD: return _switch_state("idle")
 	_set_facing_dir()
@@ -118,12 +193,13 @@ func _jump_squat_state() -> void:
 	if sprite.frame == _jump_squat_last_frame_index and not sprite.is_playing():
 		return _switch_state("jump")
 	# Apply reduced friction and gravity to keep momentum and height for jump
-	_apply_ground_friction(0.5)
-	_apply_gravity(true)
+	_apply_ground_friction(JUMP_SQUAT_GROUND_FRICTION_SCALE)
+	_apply_gravity(JUMP_SQUAT_GRAVITY_SCALE)
 	_handle_jump_hold_check()
 
 func _jump_state() -> void:
 	if is_on_floor(): return _switch_state("landing")
+	if _handle_wall_hold_check(): return
 	if velocity.y > 0: return _switch_state("fall")
 	_handle_jump_hold_check()
 	if _jump_held and _jumpTimer <= MAX_JUMP_TIME:
@@ -135,17 +211,29 @@ func _jump_state() -> void:
 
 func _fall_state() -> void:
 	if is_on_floor(): return _switch_state("landing")
+	if _handle_wall_hold_check(): return
+	if _disable_turn_till_fall : _disable_turn_till_fall = false
 	_set_facing_dir()
 	_apply_drift_speed()
 	#coyote time!
 	if _coyoteTimer < COYOTE_TIME:
 		_coyoteTimer += _engineDelta
-		_handle_jump_press()
+		if _handle_jump_press(): return
 	else:
 		_apply_gravity()
+
+func _wall_hold_state() -> void:
+	if is_on_floor(): return _switch_state("idle")
+	if not is_on_wall(): return _switch_state("fall")
+	if _handle_jump_press(): return
+	_set_facing_dir()
+	_apply_drift_speed()
+	_apply_gravity(WALL_HOLD_GRAVITY_SCALE, MAX_WALL_HOLD_FALL_SPEED)
+	#apply subtle drift into wall
+	velocity.x += WALL_HOLD_DRIFT_SPEED * _engineDelta * (1.0 if _wall_to_right else -1.0)
 
 func _landing_state() -> void:
 	if sprite.frame == _landing_anim_last_frame_index and not sprite.is_playing():
 		return _switch_state("idle")
-	_apply_ground_friction()
+	_apply_ground_friction(LANDING_GROUND_FRICTION_SCALE)
 #endregion
